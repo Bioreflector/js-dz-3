de09622ff074fa7e6f2610314667db97f81d8466
Alexandr <bioreflector@gmail.com>
2022-07-21 15:28:05 +0300

    Put it into the body

--- script.js
+++ script.js
@@ -2,3 +2,6 @@
 const wrapper = document.createElement('div')
 // add a class of wrapper to it
 wrapper.classList.add('wrapper')
+// put it into the body
+const body = document.querySelector('body')
+body.insertAdjacentElement('afterbegin', wrapper)
